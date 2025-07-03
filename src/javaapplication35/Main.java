d3ce0f19d68c350cff9a45f165c027ca513e8b58
CtLaptop <tubin478@gmail.com>
2025-07-02 20:52:48 -0600

    se creo la clase para registrar productos

---
+++ src/javaapplication35/Main.java
@@ -0,0 +1,14 @@
+
+package javaapplication35;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+//se importo repositorios para utilizar la conexion y la utilizacion Mysql
+
+public class Main {
+
+    public static void main(String[] args) {
+            
+    }
+}
